147909b7919d8b9a5db368da20f2cddb46555aec
Lochanara <32757243+Lochanara@users.noreply.github.com>
2022-02-28 20:18:24 +0530

    Add files via upload

---
+++ Assignment 1/A/A.java
@@ -0,0 +1,13 @@
+public class A	{
+
+	public static void main (String[] args) {
+
+	}
+
+	void run(){
+
+		System.out.println("A is running");			
+
+	}
+
+}
---
+++ Assignment 1/B/B.java
@@ -0,0 +1,13 @@
+public class B extends A {
+
+	public static void main (String[] args) {
+
+	}
+
+	void run(){
+
+		System.out.println("B is running");			
+
+	}
+
+}
